1995a070e344654e768b68349d38edf8a2841c40
unknown <ashmitthawait2@gmail.com>
2024-10-17 01:51:17 +0530

    alerts for malaria - prototype

--- z_score.py
+++ z_score.py
@@ -2,16 +2,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 
-data = pd.read_csv("google_trends.csv")
-search_term = "covid"
+data = pd.read_csv("malaria.csv")
+search_term = "malaria"
 
 data['z_score'] = (data[search_term] - data[search_term].mean()) / data[search_term].std()
 
 # Set a threshold for anomaly detection (e.g., |z_score| > 1.5)
-anomaly_threshold = 1.2
+anomaly_threshold = 1.5
 data['anomaly'] = np.where(data['z_score'].abs() > anomaly_threshold, 1, 0)
 
 data_outbreak = data[data['anomaly'] == 1]
+print(data_outbreak)
+
 plt.figure(figsize=(10, 6))
 plt.bar(data_outbreak['date'], data_outbreak[search_term], color='blue', linewidth=2)
 
