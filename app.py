0eaaf83fc43c4a53b25e15d2a94444c51779d8ee
unknown <ashmitthawait2@gmail.com>
2024-10-18 03:43:40 +0530

    minor interface changes

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 st.set_page_config(page_title = "Epidemic Outbreak")
 
-data = pd.read_csv("google_trends.csv")
+data = pd.read_csv("malaria.csv")
 emails = pd.read_csv("emails.csv")
 
 st.header(":red[Epidemic] Outbreak Alerts")
-st.write("The following plot shows the search interest for Covid during the past 5 years.")
+st.write("The following plot shows the search interest for Malaria during the past 5 years. It is visible that spikes are formed when the search interest crosses 40.")
 
-st.line_chart(data, x='date', y='covid', y_label="Search Interest (0-100)")
+st.line_chart(data, x='date', y='malaria', y_label="Search Interest (0-100)")
 
 st.write("Enter your Email-ID below to receive outbreak alerts!")
 email = st.text_input("Your email:")
